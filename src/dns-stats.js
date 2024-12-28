a0a8cc326f449c2348c98c9952e637abeac0ca41
Liubou Levitskaya <lyubasaraeva@gmail.com>
2024-12-28 19:34:02 +0300

    feat: implement getDNSStats function

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+let obj = {};
+  for (const domain of domains) {
+    let arr = domain.split('.');
+    let str = '';
+    for (let i = arr.length - 1; i >= 0; i--) {
+      str += '.' + arr[i];
+      obj[str] = obj[str] ? obj[str] + 1 : 1;
+    }
+  }
+  return obj;
 }
 
 module.exports = {
