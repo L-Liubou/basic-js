01d2d6ecc78cd7cd0f33b880ce138bc6f5a63530
Liubou Levitskaya <lyubasaraeva@gmail.com>
2024-12-29 18:33:29 +0300

    feat: implement dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+  const currentActivity = parseFloat(sampleActivity);
+  if (isNaN(currentActivity) || currentActivity <= 0 || currentActivity > MODERN_ACTIVITY) return false;
+
+  const decayConst = Math.log(2) / HALF_LIFE_PERIOD;
+  const age = Math.ceil(Math.log(MODERN_ACTIVITY / currentActivity) / decayConst);
+  return age;
 }
 
 module.exports = {
